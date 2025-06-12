ba9b68b3fb72c1dcd072ab0ae8aef0b445d1ea29
Yuan Chiu <chyuaner@gmail.com>
2025-06-12 21:24:27 +0800

    fix pjax for giscus comments system

---
+++ source/comments/giscus.js
@@ -0,0 +1,36 @@
+(() => {
+    const loadComments = async () => {
+        const giscusContainer = document.getElementById('giscus_container');
+        if (!giscusContainer) return;
+
+        // 清空 iframe
+        while (giscusContainer.firstChild) {
+            giscusContainer.removeChild(giscusContainer.firstChild);
+        }
+
+        // 重建 script
+        const script = document.createElement('script');
+        script.src = 'https://giscus.app/client.js';
+        script.setAttribute('data-repo', 'chyuaner/blog.yuaner.tw');
+        script.setAttribute('data-repo-id', 'R_kgDOO5dOzg');
+        script.setAttribute('data-category', 'Announcements');
+        script.setAttribute('data-category-id', 'DIC_kwDOO5dOzs4CrZfX');
+        script.setAttribute('data-mapping', 'pathname');
+        script.setAttribute('data-strict', '0');
+        script.setAttribute('data-reactions-enabled', '1');
+        script.setAttribute('data-emit-metadata', '0');
+        script.setAttribute('data-input-position', 'top');
+        script.setAttribute('data-theme', 'preferred_color_scheme');
+        script.setAttribute('data-lang', 'zh-TW');
+        script.setAttribute('data-loading', 'lazy');
+        script.setAttribute('crossorigin', 'anonymous');
+        script.setAttribute('async', '');
+
+        giscusContainer.appendChild(script);
+    };
+
+    window.loadComments = loadComments;
+    window.addEventListener('pjax:success', () => {
+        window.loadComments = loadComments;
+    });
+})();
