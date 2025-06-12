8affd61c6e6b3332aebf6228af9d9a0c5c04d76e
Yuan Chiu <chyuaner@gmail.com>
2025-06-12 22:59:55 +0800

    feat: giscus comments count

--- source/comments/giscus.js
+++ source/comments/giscus.js
@@ -1,5 +1,6 @@
 (() => {
     const loadComments = async () => {
+        // Giscus 留言系統主程式 ------------------------------------------------------
         const giscusContainer = document.getElementById('giscus_container');
         if (!giscusContainer) return;
 
@@ -22,13 +23,30 @@
         script.setAttribute('data-input-position', 'bottom');
         script.setAttribute('data-theme', 'preferred_color_scheme');
         script.setAttribute('data-lang', 'zh-TW');
-        // script.setAttribute('data-loading', 'lazy');
         script.setAttribute('crossorigin', 'anonymous');
         script.setAttribute('async', '');
 
         giscusContainer.appendChild(script);
+
+        // 自行 Patch Giscus 留言數量 ------------------------------------------------
+        const pathname = window.location.pathname.replace(/^\/|\/$/g, '') + '/'; // 去除前後斜線，保持 Giscus 格式
+        const repo = "chyuaner/blog.yuaner.tw"; // 你的 GitHub repo
+        const category = "Blog Comments"; // 你的 category 名稱
+
+        const url = `https://giscus.app/api/discussions?repo=${encodeURIComponent(repo)}&term=${encodeURIComponent(pathname)}&category=${encodeURIComponent(category)}&number=0&strict=false&last=1`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                const count = data.discussion?.totalCommentCount ?? 0;
+                document.getElementById("giscus_count").textContent = count;
+            })
+            .catch(() => {
+                document.getElementById("giscus_count").textContent = "f";
+            });
     };
 
+    // 自動載入---------------------------------------------------------------------
     window.loadComments = loadComments;
     window.addEventListener('pjax:success', () => {
         window.loadComments = loadComments;
